2b96e036ea5f00c0e2d7be4ff111fddcc2d510c3
James Edward Cagney <45402150+jamescagney@users.noreply.github.com>
2025-04-02 20:04:04 -0700

    Delete Benny_Garcia.py

--- Benny_Garcia.py
+++
@@ -1,9 +0,0 @@
-# Benny Garcia
-#CS 162 Spring 2025
-# Week 1
-# Git Assignment
-
-# Function 1: Computes the Area of a Rectangle
-def rect_area(length, width):
-    #Returns the area of a rectangle given length and width.
-    return length * width
